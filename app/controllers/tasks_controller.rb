2f3884a40b2d11e03ac3eb630b26855ef41099ac
sushmitha02 <sushmithahgowda27@gmail.com>
2019-07-22 11:25:36 +0530

    On successful authentication should land on task dashboard page.  - Created task page.  - on click of the "add task" button show a task form in modal.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   # GET /tasks/1
@@ -24,14 +24,16 @@ class TasksController < ApplicationController
   # POST /tasks
   # POST /tasks.json
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
+        format.js
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +71,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:title, :description, :due_date, :user_id)
+      params.require(:task).permit(:title, :description)#, :due_date)
     end
 end
