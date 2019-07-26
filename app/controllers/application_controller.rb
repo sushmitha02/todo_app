05d0e4c17f8932a773d6b2cb0c30cd882be72d6e
sushmitha02 <sushmithahgowda27@gmail.com>
2019-07-26 13:33:14 +0530

    Giving connection from login page to dashboard page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
-	stored_location_for(resource) || page_dashboard_path
-end
+		stored_location_for(resource) || page_dashboard_path
+	end
 end
