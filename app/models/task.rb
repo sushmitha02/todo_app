96070be4f381563e7bc64bdb0931371425b44216
sushmitha02 <sushmithahgowda27@gmail.com>
2019-07-26 13:35:42 +0530

    Added one another field status.  - Created status field to do even after refreshing the page the card should not come back to original place.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task
   include Mongoid::Document
   field :title, type: String
+  field :status, type: String, default: 'backlog'
   field :description, type: String
   field :due_date, type: Date
   
